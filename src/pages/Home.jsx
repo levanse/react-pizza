af6c3a1dff3aaf791d5f542e0e7d0565df37e47b
levanse <levansePro@gmail.com>
2024-07-20 18:03:36 +0200

    refactor: add style for mobile

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,10 +16,12 @@ const Home = () => {
         setItems(json);
         setIsLoading(false);
       });
+
+    window.scrollTo(0, 0);
   }, []);
 
   return (
-    <>
+    <div className="container">
       <div className="content__top">
         <Categories />
         <Sort />
@@ -30,7 +32,7 @@ const Home = () => {
           ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
           : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
       </div>
-    </>
+    </div>
   );
 };
 
