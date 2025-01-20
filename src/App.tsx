709597346f8f8dd0cadc5cf0f4a36599aebfc8ff
levanse <levansePro@gmail.com>
2025-01-20 09:29:52 +0100

    feat: add library react-loadable

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,42 @@
 import { Routes, Route } from 'react-router-dom';
+import Loadable from 'react-loadable';
 
 import './scss/app.scss';
 
 import Home from './pages/Home';
 import MainLayout from './layouts/MainLayout';
-import { lazy, Suspense } from 'react';
 
-const Cart = lazy(() => import(/* webpackChunkName: "Cart" */ './pages/Cart'));
-const FullPizza = lazy(
-  () => import(/* webpackChunkName: "FullPizza" */ './pages/FullPizza'),
-);
-const NotFound = lazy(
-  () => import(/* webpackChunkName: "NotFound" */ './pages/NotFound'),
-);
+// const Cart = lazy(() => import(/* webpackChunkName: "Cart" */ './pages/Cart'));
+// const FullPizza = lazy(
+//   () => import(/* webpackChunkName: "FullPizza" */ './pages/FullPizza'),
+// );
+// const NotFound = lazy(
+//   () => import(/* webpackChunkName: "NotFound" */ './pages/NotFound'),
+// );
+
+const Cart = Loadable({
+  loader: () => import(/* webpackChunkName: "Cart" */ './pages/Cart'),
+  loading: () => <div>Идет загрузка корзины...</div>,
+});
+
+const FullPizza = Loadable({
+  loader: () => import(/* webpackChunkName: "FullPizza" */ './pages/FullPizza'),
+  loading: () => <div>Идет загрузка...</div>,
+});
+
+const NotFound = Loadable({
+  loader: () => import(/* webpackChunkName: "NotFound" */ './pages/NotFound'),
+  loading: () => <div>Идет загрузка...</div>,
+});
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route path="" element={<Home />} />
-        <Route
-          path="cart"
-          element={
-            <Suspense fallback={<div>Идет загрузка корзины...</div>}>
-              <Cart />
-            </Suspense>
-          }
-        />
-        <Route
-          path="pizza/:id"
-          element={
-            <Suspense fallback={<div>Идет загрузка...</div>}>
-              <FullPizza />
-            </Suspense>
-          }
-        />
-        <Route
-          path="*"
-          element={
-            <Suspense fallback={<div>Идет загрузка...</div>}>
-              <NotFound />
-            </Suspense>
-          }
-        />
+        <Route path="cart" element={<Cart />} />
+        <Route path="pizza/:id" element={<FullPizza />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   );
